09245fc03a0ec9cd02800a8a31cdb312a5c65e5e
Benedikt Schächner <benedikt.schaechner@gmail.com>
2026-02-20 15:49:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6326175703-311d9",
+  "appId": "1:918015142594:web:3a0d746c9763311998295b",
+  "apiKey": "AIzaSyAk7RfMjM-hQ7hck2pcUQQ7zkV1lE2o5JA",
+  "authDomain": "studio-6326175703-311d9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "918015142594"
+};
