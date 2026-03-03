9e02dab0a12684e256b44b4428779232ac0dbda1
schBenedikt <benedikt.schaechner@web.de>
2026-03-03 18:08:39 +0100

    Add Firebase environment variables for Cloudflare Workers
    - Add Firebase config as NEXT_PUBLIC_* environment variables
    - Update Firebase config to use environment variables in production
    - Resolves Firebase initialization errors in deployed application

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-6326175703-311d9",
-  "appId": "1:918015142594:web:3a0d746c9763311998295b",
-  "apiKey": "AIzaSyAk7RfMjM-hQ7hck2pcUQQ7zkV1lE2o5JA",
-  "authDomain": "studio-6326175703-311d9.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "918015142594"
+  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-6326175703-311d9",
+  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:918015142594:web:3a0d746c9763311998295b",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAk7RfMjM-hQ7hck2pcUQQ7zkV1lE2o5JA",
+  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-6326175703-311d9.firebaseapp.com",
+  "measurementId": process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
+  "messagingSenderId": process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "918015142594"
 };
